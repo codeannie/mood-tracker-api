c9074000c67a2e66bd5b2c6d0a89906ebdb6983f
Annie Chiang <devbear22@gmail.com>
2018-06-16 21:24:15 -0700

    fix: revise syntax to original & kept how passport-jwt was imported. issue was doc example asking for User.findOne

--- src/auth/auth.strategies.js
+++ src/auth/auth.strategies.js
@@ -2,26 +2,17 @@ const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const { JWT_SECRET } = require('../config');
 
-const options = {
+const jwtStrat = new JwtStrategy(
+  {
     secretOrKey: JWT_SECRET,
     // Look for the JWT as a Bearer auth header
     jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('Bearer'),
     // Only allow HS256 tokens - the same as the ones we issue
-    algorithms: ['HS256']
-};
-
-const jwtStrat = new JwtStrategy(options, function(jwt_payload, done) {
-  User.findOne({id: jwt_payload.sub}, function(err, user) {
-      if (err) {
-          return done(err, false);
-      }
-      if (user) {
-          return done(null, user);
-      } else {
-          return done(null, false);
-          // or you could create a new account - handle error? 
-      }
-  });
-});  
+    algorithms: ['HS256'],
+  },
+  (payload, done) => {
+    done(null, payload.user);
+  }
+);
 
 module.exports = { jwtStrat };
